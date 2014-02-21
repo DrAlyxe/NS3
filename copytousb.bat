17a23b6ba3da8711b3bf5795fd6c33cbbe2f9b4b
U-Ben-Laptop\Ben <Ben@Ben-Laptop.(none)>
2014-02-21 19:03:02 +0000

    stuff

---
+++ copytousb.bat
@@ -0,0 +1,5 @@
+@echo off 
+
+set source=C:\cygwin64\home\Ben\NS3\Coursework1
+set destination=G:\NS3\Coursework1
+xcopy %source% %destination% /y
---
+++ usbtocygwin.bat
@@ -0,0 +1,6 @@
+@echo off 
+
+set source=G:\NS3\Coursework1
+set destination=C:\cygwin64\home\Ben\NS3\Coursework1
+
+xcopy %source% %destination% /y
